0cd51eafad7caca323b0a65ed9d5de0fbc167252
= <ditaajipratama@protonmail.com>
2024-08-08 13:25:40 +0700

    Pattern update

--- blackdashboard/main.py
+++ blackdashboard/main.py
@@ -2,58 +2,39 @@ from core   import html
 
 static = [
     {
-        'name':'/blackdashboard/css',
-        'value':{
-            'tools.staticdir.on'    : True ,
-            'tools.staticdir.dir'   : './templates/blackdashboard/static/css' ,
-        }
+        "route":"/templates/blackdashboard/css/<filepath>",
+        "root":"./templates/blackdashboard/static/css"
     },
     {
-        'name':'/blackdashboard/demo',
-        'value':{
-            'tools.staticdir.on'    : True ,
-            'tools.staticdir.dir'   : './templates/blackdashboard/static/demo' ,
-        }
+        "route":"/templates/blackdashboard/demo/<filepath>",
+        "root":"./templates/blackdashboard/static/demo"
     },
     {
-        'name':'/blackdashboard/fonts',
-        'value':{
-            'tools.staticdir.on'    : True ,
-            'tools.staticdir.dir'   : './templates/blackdashboard/static/fonts' ,
-        }
+        "route":"/templates/blackdashboard/fonts/<filepath>",
+        "root":"./templates/blackdashboard/static/fonts"
     },
     {
-        'name':'/blackdashboard/img',
-        'value':{
-            'tools.staticdir.on'    : True ,
-            'tools.staticdir.dir'   : './templates/blackdashboard/static/img' ,
-        }
+        "route":"/templates/blackdashboard/img/<filepath>",
+        "root":"./templates/blackdashboard/static/img"
     },
     {
-        'name':'/blackdashboard/js',
-        'value':{
-            'tools.staticdir.on'    : True ,
-            'tools.staticdir.dir'   : './templates/blackdashboard/static/js' ,
-        }
+        "route":"/templates/blackdashboard/js/<filepath>",
+        "root":"./templates/blackdashboard/static/js"
     },
     {
-        'name':'/blackdashboard/scss',
-        'value':{
-            'tools.staticdir.on'    : True ,
-            'tools.staticdir.dir'   : './templates/blackdashboard/static/scss' ,
-        }
-    },
+        "route":"/templates/blackdashboard/scss/<filepath>",
+        "root":"./templates/blackdashboard/static/scss"
+    }
 ]
 
 def main(dir, page):
-
     html_template   = html.main.get_html("templates/blackdashboard/html")
     html_page       = html.main.get_html(dir)
     params_list = {
-        "template"  : html_template ["template.html"    ]   ,
+        "index"     : html_template ["index.html"       ]   ,
         "navbar"    : html_template ["navbar.html"      ]   ,
         "sidebar"   : html_template ["sidebar.html"     ]   ,
         "footer"    : html_template ["footer.html"      ]   ,
-        "container" : html_page     [ page+".html"      ]
+        "container" : html_page     [f"{page}.html"     ]
     }
     return params_list
